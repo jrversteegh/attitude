9e7e41aa4094ada34d3b02b7ed790148f3309c01
Jaap Versteegh <j.r.versteegh@gmail.com>
2025-07-03 19:38:56 +0200

    Update trix headers

--- external/trix/version.h
+++ external/trix/version.h
@@ -5,10 +5,10 @@
  * \author J.R. Versteegh <j.r.versteegh@gmail.com>
  */
 
-#define GITREV 99dd296
+#define GITREV 1cfcc52
 
 #define VERSION 0.1.0
-#define BUILD_DATE 2025-07-02
+#define BUILD_DATE 2025-07-03
 #define BUILD_TYPE Release
 
 /* #undef CXX_VER */
